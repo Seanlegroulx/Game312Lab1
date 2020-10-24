ce5b6bcd1086e6a274bce428e77e37ea1ec5ce3e
unknown <sean.legroulx2001@hotmail.com>
2020-10-24 16:34:11 -0400

    chap 5 better navigation and Aliens can Die

--- Bobblehead Wars/Assets/Alien.cs
+++ Bobblehead Wars/Assets/Alien.cs
@@ -8,6 +8,9 @@ public class Alien : MonoBehaviour
 
     public Transform target;
     private NavMeshAgent agent;
+
+    public float navigationUpdate;
+    private float navigationTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +22,20 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            agent.destination = target.position;
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
 
 
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
