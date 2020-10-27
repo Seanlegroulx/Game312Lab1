3749d355dc3e7ce50b4daed81261f263cb98bec1
unknown <sean.legroulx2001@hotmail.com>
2020-10-27 10:23:36 -0400

    chap 6 all animations completed

--- Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
     private CharacterController characterController;
+    public Animator bodyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,11 +31,13 @@ public class PlayerController : MonoBehaviour
         0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            // TODO
+            bodyAnimator.SetBool("IsMoving", false); // TODO
         }
         else
         {
+            bodyAnimator.SetBool("IsMoving", true);
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+
         }
 
         RaycastHit hit;
